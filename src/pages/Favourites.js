57526e7b9c79600cd8ea5f1bc4c5b4069d4aea82
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2022-08-05 18:25:31 +0100

    create favourites component

---
+++ src/pages/Favourites.js
@@ -0,0 +1,27 @@
+import React, { useContext } from "react"
+import { DataContext } from "../Context.js"
+import Card from "../components/Card.js"
+
+function Favourites() {
+
+  const moviesData = useContext(DataContext).likedMovies
+
+  const movies = moviesData.map((movie) => {
+    return (
+        <Card
+            key = {movie.id}
+            movie = {movie}
+        />
+        )
+  })
+
+  return (
+    <div className='home'>
+        <div className='home__container'>
+            {movies}    
+        </div>
+    </div>
+  )
+}
+
+export default Favourites
